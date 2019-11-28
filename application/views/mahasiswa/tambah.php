c0db8b56d58c5b48281870a0a0a57ecb5af1688e
luthfiazzz <luthfiazizi11@gmail.com>
2019-11-28 16:55:45 +0700

    crud progress

---
+++ application/views/mahasiswa/tambah.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                Form Tambah Data Mahasiswa
+            </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" name="nama" id="nama">
+                            <small class="form-text text-danger"><?= form_error('nama'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="nrp">NRP</label>
+                            <input type="text" class="form-control" name="nrp" id="nrp">
+                            <small class="form-text text-danger"><?= form_error('nrp'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" name="email" id="email">
+                            <small class="form-text text-danger"><?= form_error('email'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Sipil">Teknik Sipil</option>
+                            <option value="Teknik Komputer">Teknik Komputer</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Teknik Tambang">Teknik Tambang</option>
+                            </select>
+                        </div>
+                        <button type"submit" name="tambah" class="btn btn-success">Tambah</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
